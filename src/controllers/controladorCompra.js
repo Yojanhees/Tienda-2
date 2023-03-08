8d3c7629756f8ec56c6dce6be58b599f8a787ff5
desarrollo <desarrollo@academicos.cesde.edu.co>
2023-03-08 08:30:47 -0500

    se crea ampliar info

---
+++ src/controllers/controladorCompra.js
@@ -0,0 +1,12 @@
+//llamo a la memoria para acceder a los datos guardados:
+let infoProducto=JSON.parse(localStorage.getItem("producto"))
+console.log(infoProducto)
+
+let foto=document.getElementById("foto")
+foto.src=infoProducto.foto
+
+let nombre=document.getElementById("nombre")
+nombre.textContent=infoProducto.nombre
+
+let precio=document.getElementById("precio")
+precio.textContent=infoProducto.precio
--- src/controllers/controladorHome.js
+++ src/controllers/controladorHome.js
@@ -8,3 +8,19 @@ let fila=document.getElementById("fila")
 
 //Pintar los productos de la tienda
 pintarProductos(productos)
+
+//escuchando click en la fila de productos
+fila.addEventListener("click",function(evento){
+    //window.location.href="./src/views/ampliarinfo.html"
+    let datosProductoSeleccionado={}
+    let tarjeta=evento.target.parentElement
+
+    datosProductoSeleccionado.foto=tarjeta.querySelector("img").src
+    datosProductoSeleccionado.nombre=tarjeta.querySelector("h3").textContent
+    datosProductoSeleccionado.precio=tarjeta.querySelector("h5").textContent
+
+    //usaremos la memoria del pc para guardar esta info
+    localStorage.setItem("producto",JSON.stringify(datosProductoSeleccionado))
+    
+    window.location.href="../../src/views/ampliarinfo.html"
+})
